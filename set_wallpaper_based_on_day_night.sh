942e7c3c1170dcab977f66eacdc44b6a2472dd76
FelipeColona <felipedev202@gmail.com>
2022-12-21 00:59:56 -0300

    feat: add night light shift to day/night script

--- set_wallpaper_based_on_day_night.sh
+++ set_wallpaper_based_on_day_night.sh
@@ -1,7 +1,9 @@
 CURRENT_DATE=$(date +%H)
 
-if [[ $CURRENT_DATE -ge 19 ]]; then
-    feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox_astro.jpg"
-else
+if [ $CURRENT_DATE -ge 5 ] && [ $CURRENT_DATE -lt 19 ]; then
+    sct 6500
     feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox-rain.jpg" 
+else
+    sct 4000
+    feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox_astro.jpg"
 fi
