6404ba2620b0b628e36af32253a4778aa76b7934
FelipeColona <felipedev202@gmail.com>
2023-03-26 19:27:17 -0300

    feat: add debug flag and opacity change to day/night script

--- set_wallpaper_based_on_day_night.sh
+++ set_wallpaper_based_on_day_night.sh
@@ -1,9 +1,22 @@
 CURRENT_DATE=$(date +%H)
 
+while getopts d: flag
+do
+    case "${flag}" in
+        d) custom_time=${OPTARG};;
+    esac
+done
+
+if [[ $custom_time -ne 0 ]]; then
+  CURRENT_DATE=$custom_time
+fi
+
 if [ $CURRENT_DATE -ge 5 ] && [ $CURRENT_DATE -lt 19 ]; then
     sct 6500
-    feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox-leafs.jpg" 
+    feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox-leafs.jpg"
+    sed -i '3 s/0.4/0.6/' "$HOME/.alacritty.yml"
 else
-    sct 4500
-    feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox_astro.jpg"
+    sct 5000
+    feh --bg-scale "$HOME/Imagens/Gruvbox/gruvbox-forest.jpg"
+    sed -i '3 s/0.6/0.4/' "$HOME/.alacritty.yml"
 fi
